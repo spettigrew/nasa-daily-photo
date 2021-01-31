3d5d12897f05f7cf026565acc35c40e2d86119a9
spettigrew <sara.pettigrew5@gmail.com>
2021-01-30 17:15:26 -0700

    Added useEffect and JSX to App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
+// import styled from 'styled-components'
 import "./App.css";
 
+// set useStat
 function App() {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+// useEffect takes a callback in one of its parameters.
+  useEffect(() => {
+    // set the axios call for the nasa url
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then((res) => {
+        // console.log(res) check the response on the data from the axios call
+        // set state to the response data
+        setData(res.data)
+      })
+      // catch for errors in response
+      .catch(error => {
+        console.log(error)
+        setError(error)
+      })
+  },[]); 
+  console.log(data)
 
-  useEffect(async () => {
-    const data = await fetch('https://api.nasa.gov/planetary/apod');
-    return await data.json();
-  }, []);
-  
   return (
     <div className="App">
-     
+     <h1>
+       Nasa - Photo of the Day
+     </h1>
+     {error && (
+       <h2>No photo available</h2>
+     )}
+     {data && !error &&(
+       <>
+       <h4>{data.date}</h4>
+       <img src={data.url} alt="Nasa Satellite Image" />
+       <h2>{data.title}</h2>
+       <p>{data.explanation}</p>
+       </>
+     )}
     </div>
   );
 }
