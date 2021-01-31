670ebee50a47315824a0144b9cd45f39c782c51a
spettigrew <sara.pettigrew5@gmail.com>
2021-01-31 14:51:30 -0700

    Back to Nasa app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import "./App.css";
 
-const Title = styled.h1`
-
-`
-// set useState
+// set useStat
 function App() {
   const [data, setData] = useState();
   const [error, setError] = useState();
 // useEffect takes a callback in one of its parameters.
   useEffect(() => {
     // set the axios call for the nasa url
-    axios.get('https://fakestoreapi.com/products')
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then((res) => {
         // console.log(res) check the response on the data from the axios call
         // set state to the response data
@@ -30,29 +27,17 @@ function App() {
   return (
     <div className="App">
      <h1>
-       Fake Shopping Cart
+       Nasa - Photo of the Day
      </h1>
      {error && (
-       <h2>No products available</h2>
+       <h2>No photo available</h2>
      )}
      {data && !error &&(
        <>
-       {/* map over data. For every item in the array(data) */}
-       {data.map((product) => (
-        //  display the data from the product parameter
-        // console.log(product)
-        <>
-          <p>{product.category}</p>
-          <img src={product.image} alt="Item image" />
-          <h3>{product.title}</h3>
-          <p>{product.price}</p>
-          <p>{product.description}</p>
-        </>
-         ))}
-       {/* <h4>{data.date}</h4>
+       <h4>{data.date}</h4>
        <img src={data.url} alt="Nasa Satellite Image" />
        <h2>{data.title}</h2>
-       <p>{data.explanation}</p> */}
+       <p>{data.explanation}</p>
        </>
      )}
     </div>
